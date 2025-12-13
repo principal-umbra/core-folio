ef0292d797aee524411212343cda21b2fb16a883
UMBRA <principal.umbra@outlook.com>
2025-12-13 17:07:08 -0400

    Update data-loader.js

--- assets/js/data-loader.js
+++ assets/js/data-loader.js
@@ -3,18 +3,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     includeTargets.forEach(el => {
         const file = el.getAttribute("data-include");
+        if (!file) return;
 
-        fetch(`/${file}`)
+        fetch(file)
             .then(response => {
-                if (!response.ok) throw new Error(`Error loading: ${file}`);
+                if (!response.ok) {
+                    throw new Error(`Error loading component: ${file}`);
+                }
                 return response.text();
             })
             .then(data => {
                 el.innerHTML = data;
             })
             .catch(err => {
-                el.innerHTML = `<p style="color:red;">Could not load component: ${file}</p>`;
                 console.error(err);
+                el.innerHTML = `<p style="color:red;">Could not load component: ${file}</p>`;
             });
     });
 });
